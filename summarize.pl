2073b87cef052ad267500ca824e50283a064e755
Narazaka <info@narazaka.net>
2018-08-04 22:30:14 +0900

    summarize json

---
+++ summarize.pl
@@ -0,0 +1,32 @@
+use 5.20.0;
+use utf8;
+use open ":encoding(utf8)";
+use IO::Dir;
+use IO::File;
+use File::Slurp qw/read_file/;
+use File::Spec::Functions qw/catfile/;
+use JSON::XS;
+
+my $results_dir = "results";
+my $summarize_file = "results.json";
+
+my $obj = {};
+
+my $dh = new IO::Dir($results_dir) or die "cannot open dir $results_dir";
+while (my $category = $dh->read) {
+    next if $category =~ /^\./;
+    my $category_dir = catfile $results_dir, $category;
+    my $dh2 = new IO::Dir($category_dir) or die "cannot open dir $category_dir";
+    while (my $language = $dh2->read) {
+        next if $language =~ /^\./;
+        my $language_file = catfile $category_dir, $language;
+        my $time = int read_file $language_file, binmode => ":utf8";
+        $obj->{$category} = {} unless $obj->{$category};
+        $obj->{$category}->{$language} = $time;
+    }
+}
+
+my $fh = new IO::File($summarize_file, "w") or die $!;
+print $fh JSON::XS->new->utf8->pretty->encode($obj);
+$fh->close;
+
