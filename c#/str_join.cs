ae6a61a0bfedf659764dede41c77d826df50cd9b
Narazaka <info@narazaka.net>
2018-08-05 14:41:57 +0900

    C# str join

---
+++ c#/str_join.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+class Program {
+    const string Chunk = "regexp123abcbcd";
+    const int RepeatCount = 10000;
+    const int Size = 1000;
+
+    static void Join() {
+        var sb = new StringBuilder();
+        for (var i = 0; i < Size; ++i) sb.Append(Chunk);
+        sb.ToString();
+    }
+
+    static void Main() {
+        var sw = new Stopwatch();
+        sw.Start();
+        for (var i = 0; i < RepeatCount; ++i) Join();
+        sw.Stop();
+        Console.WriteLine(sw.ElapsedMilliseconds);
+    }
+}
---
+++ c#/str_join_slow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+
+class Program {
+    const string Chunk = "regexp123abcbcd";
+    const int RepeatCount = 10000;
+    const int Size = 1000;
+
+    static void Join() {
+        string.Concat(Enumerable.Repeat(Chunk, Size));
+    }
+
+    static void Main() {
+        var sw = new Stopwatch();
+        sw.Start();
+        for (var i = 0; i < RepeatCount; ++i) Join();
+        sw.Stop();
+        Console.WriteLine(sw.ElapsedMilliseconds);
+    }
+}
